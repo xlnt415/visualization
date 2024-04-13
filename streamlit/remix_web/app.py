0a5bef19c3b29562eaf7c05a3064ae701f4afa89
Woo <wsh041582@gmail.com>
2024-04-14 01:20:09 +0900

    Create app.py

---
+++ streamlit/remix_web/app.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+import seaborn as sns
+
+st.title('Remix_Web')
+st.header('Remix웹으로 사용하기')
+
